ab33f850e119fd43edcac0b69723981370dee34a
ParthKadam11 <parthkadamalt@gmail.com>
2026-08-03 16:31:01 +0530

    Add axios dependency and enhance token balance retrieval API; update supported tokens to include native SOL handling and fetch current prices from Binance.

--- frontend/app/api/tokens/route.ts
+++ frontend/app/api/tokens/route.ts
@@ -1,16 +1,25 @@
-import { NextRequest } from "next/server";
-import {getAccount, getAssociatedTokenAddress } from "@solana/spl-token"
-import { connection, SUPPORTED_TOKENS } from "@/lib/constants";
-import { PublicKey } from "@solana/web3.js";
+import { NextRequest, NextResponse } from "next/server";
+import {getAccount, getAssociatedTokenAddress, getMint } from "@solana/spl-token"
+import { connection, getSupportedTokens } from "@/lib/constants";
+import {  LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
 async function getAccountBalance(token:{
     name:string,
-    mint:string
+    mint:string,
+    native:boolean
 },address:string) {
+    if(token.native){
+        const balance = await connection.getBalance( new PublicKey(address))
+        return balance / LAMPORTS_PER_SOL
+    }
+
     //ata = associated token account
     //pda = program derived address
     const ata = await getAssociatedTokenAddress(new PublicKey(token.mint),  new PublicKey(address))
     const account = await getAccount(connection,ata)    
+    const mint = await getMint(connection,new PublicKey(token.mint))
+
+    return Number(account.amount) / (10** mint.decimals)
 }
 
 
@@ -22,9 +31,16 @@ function getPrice(){
 export async function GET(req:NextRequest){
     const {searchParams} = new URL(req.url)
     const address= searchParams.get('address') as string   
-    const balance = await Promise.all(SUPPORTED_TOKENS.map(token=> getAccountBalance(token, address)))
+    const supportedTokens = await getSupportedTokens()
+    const balances = await Promise.all(supportedTokens.map(token=> getAccountBalance(token, address)))
     
+    return NextResponse.json({
+        token: supportedTokens.map((token,index)=> ({
+            ...token,
+            balance:balances[index]
 
+        }))
+    })
 
 }
 
--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,27 +1,50 @@
 import { Connection } from "@solana/web3.js"
+import axios from "axios"
 
 const TOKEN_PRICE_REFRESH_INTERVAL = 60*1000; //every 1min
-const LAST_UPDATED= null
-
+let LAST_UPDATED:number|null = null
+let prices:{[key:string]:{
+    price:string
+}}= {
+    
+}
 
 export const SUPPORTED_TOKENS:{
     name:string,
-    mint:string
+    mint:string,
+    native:boolean
 }[] = [{
     name:"USDC",
     mint:"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    native:false
 },{
     name:"USDT",
     mint:"Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
-}]
+    native:false
+},{
+    name:"SOL",
+    mint:"So11111111111111111111111111111111111111112",
+    native:true
+}
+]
 
 export const connection = new Connection("https://api.devnet.solana.com")
 
 
-export function getSupportedTokens(){
+export async function getSupportedTokens(){
     if(!LAST_UPDATED || new Date().getTime() - LAST_UPDATED < TOKEN_PRICE_REFRESH_INTERVAL){
-
-    }
+        const response = await  axios.get("https://api.binance.com/api/v3/ticker/price", {
+            params: {
+                symbols: '["SOLUSDT","SOLUSDC","USDCUSDT"]'
+            }
+        })
+        prices=response.data.data
+        LAST_UPDATED=  new Date().getTime()
+    } 
+    return SUPPORTED_TOKENS.map(s=>({
+        ...s,
+        price:prices[s.name]
+    }))
 }
 
 getSupportedTokens()
